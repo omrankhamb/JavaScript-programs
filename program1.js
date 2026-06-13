5d7e7da9582eb3f04b4524a06cf685c2ceeacea1
omrankhamb <rankhambomprasad63@gmail.com>
2026-06-13 15:26:37 +0530

    JavaScript Programs

---
+++ program1.js
@@ -0,0 +1,31 @@
+// Varaiable in Python
+var a = 23                                         // Globally scoped Variable
+console.log(a)
+
+var a = 24
+console.log(a)
+
+var a = "omprasad rankhamb"
+console.log(a)
+
+
+let iNo = 12
+console.log(iNo)
+
+//let iNo = "Omprasad Rankhamb"     ERROR                  // can Not able to declare again and again
+console.log(iNo)
+
+additin()
+
+function additin(){
+    console.log(`Hello World`,a,iNo);                   // Var a Is Globally scoped Variable
+    
+}
+
+const number = 12
+//number  = 13  not Allowed
+console.log(number)
+
+
+
+
